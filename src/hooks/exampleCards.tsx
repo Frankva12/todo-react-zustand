3fe1b28cad990a1e4d1ef07105700e4ad0e4a9e3
Frank <stanleyvasconcelos0@gmail.com>
2023-03-08 15:40:10 -0600

    ✅ add data

--- src/hooks/exampleCards.tsx
+++ src/hooks/exampleCards.tsx
@@ -9,12 +9,66 @@ const exampleCard = [
       isCompleted: false,
     },
     {
+      id: "5b155312-c54c-45ad-96e7-183b44f60ef1",
+      title: "Hello There",
+      personAssigned: "General Kenobi",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: false,
+    },
+    {
+      id: "5b155312-c54c-45ad-96e7-183b44f60ef1",
+      title: "Hello There",
+      personAssigned: "General Kenobi",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: false,
+    },
+    {
+      id: "5b155312-c54c-45ad-96e7-183b44f60ef1",
+      title: "Hello There",
+      personAssigned: "General Kenobi",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: false,
+    },
+    {
       id: "8a448fd3-d07e-4e4f-af00-3bd333f72590",
       title: "This is random",
       personAssigned: "Me",
       description: "This is a note of an example that we want to put in here, to make an example.",
       dateOfFinish: "2023-02-09",
       isCompleted: false,
+    },
+    {
+      id: "10f82b26-55f3-47c2-89e0-96fd52ec75db",
+      title: "Random Todo",
+      personAssigned: "Francisco Vasconcelos",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: true,
+    },
+    {
+      id: "10f82b26-55f3-47c2-89e0-96fd52ec75db",
+      title: "Random Todo",
+      personAssigned: "Francisco Vasconcelos",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: true,
+    },
+    {
+      id: "10f82b26-55f3-47c2-89e0-96fd52ec75db",
+      title: "Random Todo",
+      personAssigned: "Francisco Vasconcelos",
+      description:
+        "This is a note of an example that we want to put in here, to make an example.",
+      dateOfFinish: "2023-03-31",
+      isCompleted: true,
     },
     {
       id: "10f82b26-55f3-47c2-89e0-96fd52ec75db",
